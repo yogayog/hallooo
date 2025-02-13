617974e2f9185052e17c7b15404b4f3f16de9939
Yoga Aditya <148540116+yogayog@users.noreply.github.com>
2025-02-13 06:06:20 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Fungsi untuk menampilkan ucapan tambahan
+function showMessage() {
+    const message = document.getElementById("extraMessage");
+    message.style.display = "block"; // Menampilkan pesan tambahan saat tombol diklik
+}
